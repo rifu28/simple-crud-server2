f64b4a0e974a385cbeca3844f8a3e85d2a57fc59
rifu28 <rifatmeumeu28@gmail.com>
2024-09-11 00:32:06 +0600

    ajke abar

--- index.js
+++ index.js
@@ -24,6 +24,12 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    app.post("/users", (req, res) => {
+      const user = req.body;
+      console.log(user);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
@@ -31,7 +37,7 @@ async function run() {
     );
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
